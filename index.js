786d9a8a37da53602819b1f0c1ac036814eaccf5
DerrickOngoma <Odrumz64@gmail.com>
2024-03-27 14:58:49 +0300

    Destructuring

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
+
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r,o,y,g,b,indg,v] = colors;
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -8,7 +17,8 @@ const muppet = {
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
-};
+}
+const {muppetName,color,song,job,partner} = muppet
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +34,7 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { album:{theMuppetMovie:{song2,song4}},nestedJob,nestedPartner} = nestedMuppet
 
 // Strings
 
